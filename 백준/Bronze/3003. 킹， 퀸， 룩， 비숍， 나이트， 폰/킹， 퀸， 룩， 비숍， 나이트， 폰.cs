e379937de531fc4be71ba43dec35ff5796370abb
JiwonEE2 <110376350+JiwonEE2@users.noreply.github.com>
2025-04-03 14:39:29 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 64 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cs
@@ -0,0 +1,2 @@
+string[] s=Console.ReadLine().Split();
+Console.WriteLine($"{1-int.Parse(s[0])} {1-int.Parse(s[1])} {2-int.Parse(s[2])} {2-int.Parse(s[3])} {2-int.Parse(s[4])} {8-int.Parse(s[5])}");
