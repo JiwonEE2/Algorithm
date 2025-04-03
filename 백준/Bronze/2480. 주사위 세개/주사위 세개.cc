daa09dcf9a235b2277d3cef54af6b2e0ae846c30
JiwonEE2 <110376350+JiwonEE2@users.noreply.github.com>
2025-04-03 17:28:19 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,21 +1,17 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int main(){
-    int a,b,c,m,h;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b,c;
     cin>>a>>b>>c;
-    if((a!=b)&&(a!=c)&&(b!=c)){
-        if(a>b){
-            if(a>c){h=a;}
-            else{h=c;}
-        }
-        else{
-            if(b>c){h=b;}
-            else{h=c;}
-        }
-        m=h*100;
+    if(a==b){
+        if(b==c)cout<<10000+a*1000;
+        else cout<<1000+a*100;
     }
-    else if(a==b&&b==c){m=10000+a*1000;}
-    else if(a==b||a==c){m=1000+a*100;}
-    else{m=1000+b*100;}
-    cout<<m;
+    else if(a==c)cout<<1000+a*100;
+    else if(b==c)cout<<1000+b*100;
+    else cout<<max(max(a,b),c)*100;
 }
