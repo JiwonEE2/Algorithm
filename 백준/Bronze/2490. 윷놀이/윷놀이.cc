768340f910af07c12450e922974256362a51afd6
JiwonEE2 <110376350+JiwonEE2@users.noreply.github.com>
2025-04-07 11:47:48 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -4,16 +4,19 @@ using namespace std;
 int result, input;
 string res = "DCBAE";
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-    
-  for(int r = 0; r < 3; r++) {
-    result = 0;
-    for(int c = 0; c < 4; c++) {
-      cin >> input;
-      result += input;
-    }
-    cout << res[result] << '\n';
-  }
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	for (int i = 0; i < 3; i++)
+	{
+		result = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> input;
+			result += input;
+		}
+		cout << res[result] << '\n';
+	}
 }
