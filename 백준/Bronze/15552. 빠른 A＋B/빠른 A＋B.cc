a870cac4a8e67f65fa510bfdbcb245fa6f4662be
JiwonEE2 <110376350+JiwonEE2@users.noreply.github.com>
2025-04-16 10:22:58 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,13 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int T;
-    cin>>T;
-    int A[T],B[T];
-    for(int i=0;i<T;i++){
-        cin>>A[i]>>B[i];
-    }
-    for(int i=0;i<T;i++){
-        cout<<A[i]+B[i]<<"\n";
-    }
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T, A, B;
+	vector<int> sum;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+		sum.push_back(A + B);
+	}
+    
+	for (int i = 0; i < T; i++)
+	{
+		cout << sum[i] << '\n';
+	}
 }
