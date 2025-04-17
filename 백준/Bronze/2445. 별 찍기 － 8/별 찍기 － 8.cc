b340c1d1451f8aeea14c1e2810c1b42c99f54d4f
JiwonEE2 <110376350+JiwonEE2@users.noreply.github.com>
2025-04-17 14:39:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	for (int i = -N + 1; i < N; i++)
+	{
+		int j = 0;
+		for (; j < N - abs(i); j++)
+		{
+			cout << '*';
+		}
+		for (j = 0; j < abs(i) * 2; j++)
+		{
+			cout << ' ';
+		}
+		for (j = 0; j < N - abs(i); j++)
+		{
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
